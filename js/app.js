ba8ae73a0a269ebbe8e1ec55666c8af5796d6489
Maxence Warzecha <warmax@gmail.com>
2012-08-30 11:33:22 +0300

    Update js/app.js
    get the model value to watch from the tag ng-options

--- js/app.js
+++ js/app.js
@@ -2,10 +2,21 @@ var module = angular.module('myApp', []);
 
 module.directive('chosen',function(){
     var linker = function(scope,element,attr) {
-        scope.$watch('recipientsList',function(){
-            element.trigger('liszt:updated');
-        })
-        element.chosen();
+        if(attr.ngOptions){
+        	
+    		//Get the name of the source list by getting the last String
+    		nhOptionVals = attr.ngOptions.split(" ");
+    		var sourceList = nhOptionVals[nhOptionVals.length - 1];
+
+    		//We watch the source (each time this value change, then we trigger the event 'liszt:updated'
+    		scope.$watch(sourceList,function(){
+    			//See Updating Chosen Dynamically
+                element.trigger('liszt:updated'); //Trigger event defined by Chosen Plugin (see Chosen documenation)
+            })
+            
+            //Apply the plugin
+            element.chosen();
+    	}
     };
 
     return {
